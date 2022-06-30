3e9ae57f80eb416372b81b2e1ada95f3e0420dc1
Sofia <sofiasinoti@gmail.com>
2022-06-30 01:37:45 +0100

    new commits

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ function GoCreateQuizz(){
     <h1>Comece pelo começo</h1>
     <div class="BoxItensCreate">
         <input class="TitleCreate" type="text" placeholder="Título do seu quizz">
-        <input class="UrlCreate" type="text" placeholder="URL da imagem do seu quizz">
-        <input class="QuestionCountCreate" type="text" placeholder="Quantidade de perguntas do quizz">
-        <input class="LevelCountCreate" type="text" placeholder="Quantidade de níveis do quizz">
+        <input class="UrlCreate" type="url" placeholder="URL da imagem do seu quizz">
+        <input class="QuestionCountCreate" type="number" min="3" value="3" step="1" placeholder="Quantidade de perguntas do quizz">
+        <input class="LevelCountCreate" type="number" min="2" value="2" step="1" placeholder="Quantidade de níveis do quizz">
     </div>
     <div class="ButtonContinue" onclick="GoQuestionCreate()">
         <h3>Prosseguir pra criar perguntas</h3>
