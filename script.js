4395ae657c2fb7fe73987dc4b76671df58e0ab37
Sofia <sofiasinoti@gmail.com>
2022-07-01 01:07:39 +0100

    button upgrade

--- script.js
+++ script.js
@@ -14,7 +14,35 @@ function GoCreateQuizz(){
 </div>`;
     document.querySelector(".PageContent").innerHTML += DomCreateQuizz;
 }
+
+let TitleCreateItem;
+let UrlCreateItem;
+let QuestionCountCreateItem;
+let LevelCountCreateItem;
+
+
 function GoQuestionCreate(){
+    let QuestionItemsValue = document.querySelectorAll("input").value;
+    TitleCreateItem =QuestionItemsValue[0]
+    UrlCreateItem =QuestionItemsValue[1]
+    QuestionCountCreateItem = QuestionItemsValue[2]
+    LevelCountCreateItem = QuestionItemsValue[3]
+    
+    let DomCreateQuizz =`
+    <div class="ContentCreate">
+    <h1>Comece pelo começo</h1>
+    <div class="BoxItensCreate">
+        <input class="TitleCreate" onchange="ValidateInput(this)" type="text" placeholder="Título do seu quizz">
+        <input class="UrlCreate" onchange="ValidateInput(this)" type="url" placeholder="URL da imagem do seu quizz">
+        <input class="QuestionCountCreate" onchange="ValidateInput(this)" type="number" min="3" value="3" step="1" placeholder="Quantidade de perguntas do quizz">
+        <input class="LevelCountCreate" onchange="ValidateInput(this)" type="number" min="2" value="2" step="1" placeholder="Quantidade de níveis do quizz">
+    </div>
+    <div class="ButtonContinue" onclick="GoQuestionCreate()">
+        <h3>Prosseguir pra criar perguntas</h3>
+    </div>
+</div>`;
+    document.querySelector(".PageContent").innerHTML += DomCreateQuizz;
+
     
 }
 
